39daa4db1e9fae98a3ad833bd10df80ce440a576
mian-azam <tarbelite04@gmail.com>
2023-05-12 11:53:15 -0500

    User BLL

--- Business Logic Layer/UserBusinessLogic.cs
+++ Business Logic Layer/UserBusinessLogic.cs
@@ -1,6 +1,53 @@
-﻿namespace SD_340_W22SD_Final_Project_Group6.Business_Logic_Layer
+﻿using Microsoft.AspNetCore.Identity;
+using SD_340_W22SD_Final_Project_Group6.Data;
+using SD_340_W22SD_Final_Project_Group6.Models;
+using System.Data;
+
+namespace SD_340_W22SD_Final_Project_Group6.Business_Logic_Layer
 {
     public class UserBusinessLogic
     {
+        private readonly UserManager<ApplicationUser> _users;
+        private IRepository<ApplicationUser> _userRepository;
+
+        public UserBusinessLogic(UserManager<ApplicationUser> users, IRepository<ApplicationUser> userRepository)
+        {
+            _users = users;
+            _userRepository = userRepository;
+        }
+
+        public async void GetUserInRole(string roleName)
+        {
+            List<ApplicationUser> UsersInRole = (List<ApplicationUser>)await _users.GetUsersInRoleAsync(roleName);
+        }
+        public async List<string> GetRoleOfUser(ApplicationUser user)
+        {
+          return  await _users.GetRolesAsync(user).to;
+        }
+        public async void AssignRole(string roleName, string userId)
+        {
+            ApplicationUser user = _userRepository.Get(userId);
+
+
+            ICollection<string> roleUser = await _users.GetRolesAsync(user);
+
+
+            if (roleUser.Count == 0)
+            {
+                await _users.AddToRoleAsync(user, role);
+
+                return RedirectToAction("Index", "Admin", new { area = "" });
+
+            }
+            else
+            {
+
+                await _users.RemoveFromRoleAsync(user, roleUser.First());
+                await _users.AddToRoleAsync(user, role);
+
+
+                return RedirectToAction("Index", "Admin", new { area = "" });
+            }
+        }
     }
 }
--- Data/UserRepository.cs
+++ Data/UserRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace SD_340_W22SD_Final_Project_Group6.Data
+﻿using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.Data
 {
-    public class UserRepository
+    public class UserRepository : IRepository<ApplicationUser>
     {
+        private ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicationUser? Get(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicationUser? Get(string? id)
+        {
+            return _context.Users.First( u => u.Id == id);
+
+        }
+       
+        public ICollection<ApplicationUser> GetAll()
+        {
+            return _context.Users.ToHashSet<ApplicationUser>();
+        }
+
+        public void Update(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
